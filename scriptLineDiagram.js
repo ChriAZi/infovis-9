cabdae3444a577a0730d1bf36abbb3cafd7f09cc
ElenaWW <elenawallwitz@gmail.com>
2020-11-26 14:41:02 +0100

    initial commit Kartendarstellung

---
+++ scriptLineDiagram.js
@@ -0,0 +1,93 @@
+function filterData(data){
+    const maxTempYear = data.filter(
+        item => item.countriesAndTerritories === 'Germany' && item.year == 2020 && item.month == 11
+    );
+
+    visualiseChart(maxTempYear);
+}
+
+function visualiseChart(data){
+    d3.select("#mySlider")
+    .attr("min", 1)
+    .attr("max", 12)
+    .attr("value", 1);
+    var margin = {top: 10, right: 80, bottom: 80, left:80},
+    width = 460 - margin.left -margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+    var svg = d3.select("#visualisationContainer")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    //x-Axis
+    var xAxis = d3.scaleLinear()
+        //.domain(d3.extent(data, item=> Number(item.day)))
+        .domain([1,12])
+        .range([0, width]);
+
+    svg.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(d3.axisBottom(xAxis));
+        
+        
+    // text label for the x axis
+    svg.append("text")             
+        .attr("transform",
+            "translate(" + (width/2) + " ," + 
+                            (height + margin.top + 20) + ")")
+        .style("text-anchor", "middle")
+        .text("November")
+
+    //y-Axis
+    var yAxis = d3.scaleLinear()
+        //.domain([d3.min(data, item=>Number(item.cases)) - 1, d3.max(data, item=>Number(item.cases))])
+        //.domain([0, d3.max(data, item=>Number(item.wert))])
+        .domain([0, 23000])
+        .range([height, 0]);
+
+    svg.append("g")
+        .call(d3.axisLeft(yAxis));
+
+    // text label for the y axis
+    svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x",0 - (height / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("Cases");  
+
+    //curve
+    var curve = svg.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "turquoise")
+        .attr("stroke-width", 1.5)
+        .attr("d", d3.line()
+            .x(item => xAxis(Number(item.day)))
+            .y(item => yAxis(Number(item.cases)))
+            );
+
+   /* d3.select("#mySlider")
+        .on("change", function(d){
+            selectedValue = this.value
+            xAxis.domain([selectedValue, d3.max(data, item=>Number(item.day))])
+                .range([0, width]);
+
+            svg.select("g")
+                .attr("transform", `translate(0,${height})`)
+                .call(d3.axisBottom(xAxis));
+            
+            curve.datum(data)
+                .attr("fill", "none")
+                .attr("stroke", "red")
+                .attr("stroke-width", 1.5)
+                .attr("d", d3.line()
+                    .x(item => xAxis(Number(item.day)))
+                    .y(item => yAxis(Number(item.cases)))
+                );
+        })*/
+}
---
+++ scriptMap.js
@@ -0,0 +1,69 @@
+function setGeoData(){
+    var mapNames = [];
+
+    // The svg
+    var svg = d3.select("#my_dataviz");
+
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
+    
+    // Map and projection
+    var projection = d3.geoMercator()
+        .center([12, 50])                // GPS of location to zoom on
+        .scale(3000)                       // This is like the zoom
+        .translate([ width/2, height/2 ])
+    
+          // Load external data and boot
+    d3.json("landkreise.geojson", function(data){
+        // Draw the map
+        svg.append("g")
+            .selectAll("path")
+            .data(data.features)
+            .enter()
+            .append("path")
+            .attr("fill", function(d){
+                return getColor(d.properties.GEN);
+            })
+            .attr("id", function(d){
+                mapNames.push(d.properties.GEN)
+                return d.properties.GEN;
+            })
+            .attr("d", d3.geoPath()
+                .projection(projection)
+            )
+            .style("stroke", "black")
+            .on("mouseover", function(){
+                var col = d3.select(this).style('fill')
+                d3.select(this).style("stroke", col)
+                d3.select(this).style("fill", "white")
+            })                
+            .on("mouseout", function(){
+                var col = d3.select(this).style('stroke')
+                d3.select(this).style("stroke", "black")
+                d3.select(this).style("fill", col)
+            })
+            .on("click", function(d){
+                console.log("Click")
+                d3.select("#mapText").text(d.properties.GEN); 
+            });
+
+        console.log("ArrLength: "+mapNames.length)
+    })
+
+    function getColor(element){
+        //TODO get data for this specific element
+        //TODO replace, only for color visualisation
+        var numb = getRandomInt(3)
+        if(numb==0){
+            return "red";
+        }else if(numb==1){
+            return "orange";
+        }
+        return "yellow"
+    }
+
+    //TODO remove, after color function is implemented
+    function getRandomInt(max){
+        return Math.floor(Math.random() *Math.floor(max));
+    }
+}
