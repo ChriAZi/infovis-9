85d379e0a7a42b48ea8b7aabd8d6c86d0de86d7c
David Spormann <david.spormann@gmx.de>
2020-11-29 17:46:10 +0100

    Rename 'visualisationContainer' to 'areaChart'

--- scriptLineDiagram.js
+++ scriptLineDiagram.js
@@ -15,7 +15,7 @@ function visualiseChart(data){
     width = 460 - margin.left -margin.right,
     height = 400 - margin.top - margin.bottom;
 
-    var svg = d3.select("#visualisationContainer")
+    var svg = d3.select("#areaChart")
         .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
