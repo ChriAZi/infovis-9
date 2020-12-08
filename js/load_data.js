6a3a9c5a330152a3acf16b11deebee8dac288ba4
David Spormann <david.spormann@gmx.de>
2020-12-08 15:54:52 +0100

    Also load county dict in web client

--- js/load_data.js
+++ js/load_data.js
@@ -1,5 +1,10 @@
 var data;
+var counties;
 
 fetch('data/data.json')
     .then(res => res.json())
     .then(res => data = res);
+
+fetch('data/counties.json')
+    .then(res => res.json())
+    .then(res => counties = res);
