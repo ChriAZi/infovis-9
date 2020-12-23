96465af32fe8eefa6845daf2c86fed354b02facf
christopher@familie-voit.de <christopher@familie-voit.de>
2020-12-23 18:49:55 +0100

    Dynamic Color Setting
    * Metric now has color scale properties
    * getColor(value) returns color for currently selected Metric for given value
    * minor CSS changes
    * fixed Buttons being wrapped in form -> layout is now ugly but will be fixed soon

--- js/control.js
+++ js/control.js
@@ -4,11 +4,55 @@ const Metric = {
     TOTAL_CASES: "totalCases",
     TOTAL_DEATHS: "totalDeaths",
     CASE_INCIDENCE: "caseIncidence",
+    BED_CAPACITY: "bedCapacity",
     DEATH_INCIDENCE: "deathIncidence",
     FREE_BEDS: "freeBeds",
     OCCUPIED_BEDS: "occupiedBeds",
     MORTALITY_RATE: "mortalityRate",
-    BED_CAPACITY: "bedCapacity",
+    properties: {
+        "newCases": {
+            minValue: 0,
+            maxValue: 100,
+            baseColor: "#ffaa00",
+            scaleStartColor: "#fee3ab",
+            scaleEndColor: "#ffaa00"
+        },
+        "newDeaths": {
+            minValue: 0,
+            maxValue: 2,
+            baseColor: "#333333",
+            scaleStartColor: "#eaeaeb",
+            scaleEndColor: "#eaeaeb"
+        },
+        "totalCases": {
+            minValue: 0,
+            maxValue: 2,
+            baseColor: "#ffea4c",
+            scaleStartColor: "#ffea4c",
+            scaleEndColor: "#6c067e"
+        },
+        "totalDeaths": {
+            minValue: 0,
+            maxValue: 2,
+            baseColor: "#002ea3",
+            scaleStartColor: "#6879a1",
+            scaleEndColor: "#002ea3"
+        },
+        "caseIncidence": {
+            minValue: 0,
+            maxValue: 2,
+            baseColor: "#78121e",
+            scaleStartColor: "#ffb753",
+            scaleEndColor: "#78121e"
+        },
+        "bedCapacity": {
+            minValue: 0,
+            maxValue: 2,
+            baseColor: "#de0000",
+            scaleStartColor: "#8ab670",
+            scaleEndColor: "#de0000"
+        }
+    }
 };
 Object.freeze(Metric);
 
@@ -19,21 +63,21 @@ var selectedDate = null;
 function setMetric(metric) {
     selectedMetric = metric;
     updateAll();
-};
+}
 
 function setCounty(id) {
     selectedCountyId = id;
     // TODO: updateMap()
     // TODO: updateAreaChart()
     // TODO: updateScatterPlot()
-};
+}
 
 function setDate(date) {
     selectedDate = date;
     // TODO: updateMap()
     // TODO: updateAreaChart()
     // TODO: updateScatterPlot()
-};
+}
 
 function onDataLoaded() {
     // select latest date (last element in sorted data)
@@ -49,4 +93,11 @@ function updateAll() {
     // TODO: updateMap()
     // TODO: updateAreaChart()
     // TODO: updateScatterPlot()
+}
+
+function getColor(value) {
+    let scale = d3.scaleLinear()
+        .domain([Metric.properties[selectedMetric].minValue, Metric.properties[selectedMetric].maxValue])
+        .range([Metric.properties[selectedMetric].scaleStartColor, Metric.properties[selectedMetric].scaleEndColor]);
+    return scale(value);
 }
