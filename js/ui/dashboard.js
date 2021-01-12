e95817e9d56e3a553fd053c7c1293b78d296d52d
Christopher Voit <christopher@familie-voit.de>
2021-01-12 17:58:24 +0100

    Changes to Color Scaling

--- js/ui/dashboard.js
+++ js/ui/dashboard.js
@@ -14,26 +14,27 @@ $(document).ready(function () {
     })
 })
 
+const updateConfig = {
+    COUNTY: 'county',
+    TOTAL: 'total',
+    GENERAL: 'general'
+}
+Object.freeze(updateConfig)
+
+let selectedConfig = updateConfig.TOTAL;
+
 function updateDashboard(county) {
     $('.selected-location').html(county['GEN']);
-    $('.metric.newCases').find('.metric-number').html(data[selectedDate][county['AGS']][Metric.NEW_CASES] || 'keine Daten');
-    $('.metric.totalCases').find('.metric-number').html(data[selectedDate][county['AGS']][Metric.TOTAL_CASES] || 'keine Daten');
-    $('.metric.newDeaths').find('.metric-number').html(data[selectedDate][county['AGS']][Metric.NEW_DEATHS] || 'keine Daten');
-    $('.metric.totalDeaths').find('.metric-number').html(data[selectedDate][county['AGS']][Metric.TOTAL_DEATHS] || 'keine Daten');
-    $('.metric.caseIncidence').find('.metric-number').html(data[selectedDate][county['AGS']][Metric.CASE_INCIDENCE] || 'keine Daten');
-    $('.metric.deathIncidence').find('.metric-number').html(data[selectedDate][county['AGS']][Metric.DEATH_INCIDENCE] || 'keine Daten');
+    selectedConfig = updateConfig.COUNTY;
+    updateMetricHTML(county);
     $('.back-to-all-data').css('opacity', '1');
     setCounty(county['AGS']);
 }
 
 function setDefaultDashboardValues() {
     $('.selected-location').html('Deutschland');
-    $('.metric.newCases').find('.metric-number').html(data[selectedDate]['all'][Metric.NEW_CASES] || 'keine Daten');
-    $('.metric.totalCases').find('.metric-number').html(data[selectedDate]['all'][Metric.TOTAL_CASES] || 'keine Daten');
-    $('.metric.newDeaths').find('.metric-number').html(data[selectedDate]['all'][Metric.NEW_DEATHS] || 'keine Daten');
-    $('.metric.totalDeaths').find('.metric-number').html(data[selectedDate]['all'][Metric.TOTAL_DEATHS] || 'keine Daten');
-    $('.metric.caseIncidence').find('.metric-number').html(data[selectedDate]['all'][Metric.CASE_INCIDENCE] || 'keine Daten');
-    $('.metric.deathIncidence').find('.metric-number').html(data[selectedDate]['all'][Metric.DEATH_INCIDENCE] || 'keine Daten');
+    selectedConfig = updateConfig.TOTAL;
+    updateMetricHTML();
     $('.metric-date').html('Stand: ' + getDateInFormat(selectedDate));
     $('.back-to-all-data').css('opacity', '0');
     setCounty(null);
@@ -41,13 +42,8 @@ function setDefaultDashboardValues() {
 }
 
 function updateMetrics() {
-    let metricScope = selectedCountyId || 'all';
-    $('.metric.newCases').find('.metric-number').html(data[selectedDate][metricScope][Metric.NEW_CASES] || 'keine Daten');
-    $('.metric.totalCases').find('.metric-number').html(data[selectedDate][metricScope][Metric.TOTAL_CASES] || 'keine Daten');
-    $('.metric.newDeaths').find('.metric-number').html(data[selectedDate][metricScope][Metric.NEW_DEATHS] || 'keine Daten');
-    $('.metric.totalDeaths').find('.metric-number').html(data[selectedDate][metricScope][Metric.TOTAL_DEATHS] || 'keine Daten');
-    $('.metric.caseIncidence').find('.metric-number').html(data[selectedDate][metricScope][Metric.CASE_INCIDENCE] || 'keine Daten');
-    $('.metric.deathIncidence').find('.metric-number').html(data[selectedDate][metricScope][Metric.DEATH_INCIDENCE] || 'keine Daten');
+    selectedConfig = updateConfig.GENERAL;
+    updateMetricHTML();
 }
 
 function removeSelection() {
@@ -61,4 +57,24 @@ function getDateInFormat(date) {
     let parseDateFromData = d3.timeParse('%Y/%m/%d');
     let readableDateParser = d3.timeFormat('%d.%m.%Y');
     return readableDateParser(parseDateFromData(date));
+}
+
+function updateMetricHTML(county) {
+    let config;
+    switch (selectedConfig) {
+        case updateConfig.COUNTY:
+            config = county['AGS'];
+            break;
+        case updateConfig.TOTAL:
+            config = 'all'
+            break;
+        case updateConfig.GENERAL:
+            config = selectedCountyId || 'all';
+    }
+    $('.metric.newCases').find('.metric-number').html(data[selectedDate][config][Metric.NEW_CASES] || 'keine Daten');
+    $('.metric.totalCases').find('.metric-number').html(data[selectedDate][config][Metric.TOTAL_CASES] || 'keine Daten');
+    $('.metric.newDeaths').find('.metric-number').html(data[selectedDate][config][Metric.NEW_DEATHS] || 'keine Daten');
+    $('.metric.totalDeaths').find('.metric-number').html(data[selectedDate][config][Metric.TOTAL_DEATHS] || 'keine Daten');
+    $('.metric.caseIncidence').find('.metric-number').html(Math.round(data[selectedDate][config][Metric.CASE_INCIDENCE] * 10) / 10 || 'keine Daten');
+    $('.metric.deathIncidence').find('.metric-number').html(Math.round(data[selectedDate][config][Metric.DEATH_INCIDENCE] * 10) / 10 || 'keine Daten');
 }
