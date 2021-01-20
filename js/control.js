6d50412b7803de67258009e5681b9fbb0249e2fa
Christopher Voit <christopher@familie-voit.de>
2021-01-20 22:04:57 +0100

    Metric Refactor
    * Death Incidence -> Lethality Rate
    * Made Adaptions in Dashboard, Map and Scatterplot

--- js/control.js
+++ js/control.js
@@ -64,32 +64,7 @@ var selectedMetric = Metric.CASE_INCIDENCE;
 var selectedCountyId = null;
 var selectedDate = null;
 
-var germanFormatters = d3.timeFormatDefaultLocale({
-    "decimal": ",",
-    "thousands": ".",
-    "grouping": [3],
-    "currency": ["€", ""],
-    "dateTime": "%a %b %e %X %Y",
-    "date": "%d.%m.%Y",
-    "time": "%H:%M:%S",
-    "periods": ["AM", "PM"],
-    "days": ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"],
-    "shortDays": ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"],
-    "months": ["Jannuar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
-    "shortMonths": ["Jan", "Feb", "Mär", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"]
-});
-
-var customTimeFormat = germanFormatters.timeFormat.multi([
-    [".%L", function(d) { return d.getMilliseconds(); }],
-    [":%S", function(d) { return d.getSeconds(); }],
-    ["%I:%M", function(d) { return d.getMinutes(); }],
-    ["%Hh", function(d) { return d.getHours(); }],
-    ["%B", function(d) { return d.getMonth(); }],
-    ["%Y", function() { return true; }]
-]);
-
 function setMetric(metric) {
-
     selectedMetric = metric;
     updateAll();
 }
@@ -143,7 +118,6 @@ function setMinMaxValuesForMetricObject() {
 function getMinMaxInCounties(metric) {
     let tmpMin, tmpMax;
     tmpMin = tmpMax = 0;
-    let test;
     for (let date in data) {
         for (let county in data[date]) {
             if (county !== 'all') {
