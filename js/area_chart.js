49eb2cbe0d541966b09a15ae61eede7d78ea4ae2
aminadacikj <74914995+aminadacikj@users.noreply.github.com>
2021-01-23 00:18:04 +0100

    Add line graph showing metric in area chart

--- js/area_chart.js
+++ js/area_chart.js
@@ -25,6 +25,10 @@ let stack;
 let stackedData;
 let xValue;
 
+let dataDates;
+let lineChart;
+let dynamicLine;
+
 let bedOccData;
 let countyBasedData;
 
@@ -147,6 +151,17 @@ function initAreaChart() {
                 .attr('dy', '1em')
                 .style('text-anchor', 'middle')
                 .text('Anzahl Betten');
+        
+            dataDates = Object.keys(data);
+            lineChart = d3.line()
+                .x(function (d) {
+                    d = new Date(d);
+                    return xScale(d)
+                })
+                .y(function (d) {
+                    return yScale(data[d][selectedCountyId][selectedMetric]);
+                })
+
         })
 
 }
@@ -180,7 +195,8 @@ function updateAreaChart() {
             .transition().duration(60)
             .style("opacity", 1)
             .attr("d", d => area(d));
-
+        
+        d3.select("path.line").remove();
     }
 
     lineV
@@ -231,7 +247,26 @@ function updateAreaCountyBased() {
                 .transition().duration(1000)
                 .style("opacity", 1)
                 .attr("d", d => area(d));
+            dynamicLine = chart.append("path")
+                .attr('class', 'line')
+                // .attr("d", lineChart(dataDates))
+                .attr("fill", "none")
+                //.attr("stroke", metricColor)
+                .attr("stroke-width", 2);
 
+            chart.select('.line').datum(dataDates)
+                .transition()
+                .duration(1000)
+                .attr("stroke", metricColor)
+                .attr("d", d3.line()
+                    .x(function (d) {
+                        d = new Date(d);
+                        return xScale(d)
+                    })
+                    .y(function (d) {
+                        return yScale(data[d][selectedCountyId][selectedMetric]);
+                    })
+                )
         })
 }
 
