2b47195647eb6f29bd8aa89b973658dfc9681cc6
aminadacikj <74914995+aminadacikj@users.noreply.github.com>
2021-01-31 18:01:53 +0100

    Update area_chart.js
    Add functionality for line graph germany based

--- js/diagrams/area_chart.js
+++ js/diagrams/area_chart.js
@@ -70,14 +70,14 @@ async function initAreaChart() {
 
     grp = chart.append('g');
 
-// Create stack
+    // Create stack
     stack = d3.stack().keys(['Belegte', 'Freie', 'Notfallreserve']);
     stackedData = stack(data); // Create scales
     xValue = d => d.date;
 
-//x-Axis
+    //x-Axis
     xScale = d3.scaleTime()
-        .domain(d3.extent(data, xValue))
+        .domain(d3.extent(domain))
         .range([0, width]);
     xAxisGerman = d3.axisBottom(xScale).tickFormat(d3.timeFormat("%b"));
 
@@ -118,14 +118,14 @@ async function initAreaChart() {
         .attr('stroke-width', 0.5)
         .attr('d', d => area(d));
 
-// Add the X Axis
+    // Add the X Axis
     xAxis = chart
         .append('g')
         .attr('transform', `translate(0,${height})`)
         .attr('id', 'area-chart-x-axis')
         .call(xAxisGerman);
 
-// Add the Y Axis
+    // Add the Y Axis
     yAxis = chart
         .append('g')
         .attr('transform', `translate(${width}, 0)`)
@@ -137,7 +137,7 @@ async function initAreaChart() {
         .attr('display', 'none')
         .call(d3.axisLeft(yScaleLine));
     
-// Add line
+    // Add line
     lineV = grp
         .append('line')
         .datum(data)
@@ -148,7 +148,7 @@ async function initAreaChart() {
         .attr('stroke', 'black')
         .style('stroke-width', 1);
 
-// text label for the x axis
+    // text label for the x axis
     chart.append('text')
         .attr('transform',
             'translate(' + (width / 2) + ' ,' +
@@ -156,7 +156,7 @@ async function initAreaChart() {
         .style('text-anchor', 'middle')
         .text('Zeit');
 
-// text label for the y axis
+    // text label for the y axis
     chart.append('text')
         .attr('transform', 'rotate(-90)')
         .attr('y', width + margin.left)
@@ -173,6 +173,7 @@ async function initAreaChart() {
         .style('text-anchor', 'middle');
     
     lineChart = d3.line();
+    updateAreaChart();
 }
 
 function updateAreaChart() {
@@ -204,9 +205,38 @@ function updateAreaChart() {
             .style('opacity', 1)
             .attr('d', d => area(d));
         
-        yAxisLine.attr('display', 'none');
-        yAxisLineText.attr('display', 'none');
-        d3.select('path.line').remove();
+        dynamicLine = chart.append('path')
+            .attr('class', 'line')
+            .attr('fill', 'none')
+            .attr('stroke-width', 3);
+
+        yScaleLine.domain([0, getMaxValue()]);
+        yAxisLine.transition().duration(500).ease(d3.easeLinear).attr('display', 'block')
+            .call(d3.axisLeft(yScaleLine));
+
+        yAxisLineText.text(Metric.properties[selectedMetric].name).attr('display', 'block');
+
+
+        chart.select('.line')
+            .datum(dataDates)
+            .transition()
+            .duration(1000)
+            .attr('stroke', Metric.properties[selectedMetric].baseColor)
+            .attr('d', d3.line()
+                .x(function (d) {
+                    d = new Date(d);
+                    return xScale(d)
+                })
+                .y(function (d) {
+                    if (selectedMetric === 'lethalityRate') {
+                        let rate = getLethalityRate('all', d);
+                        let lethValue = Math.round(((rate * 100) + Number.EPSILON) * 100) / 100;
+                        return yScaleLine(lethValue)
+                    } else {
+                        return yScaleLine(data[d]['all'][selectedMetric]);
+                    }
+                })
+            )
     }
 
     lineV
@@ -239,7 +269,7 @@ async function updateAreaCountyBased() {
     yAxis.transition().duration(1500).ease(d3.easeLinear)
         .call(d3.axisRight(yScale));
     
-    yScaleLine.domain([0, getMaxValue(selectedMetric)]);
+    yScaleLine.domain([0, getMaxValue()]);
     yAxisLine.transition().duration(1000).ease(d3.easeLinear).attr('display', 'block')
         .call(d3.axisLeft(yScaleLine));
     
@@ -266,7 +296,6 @@ async function updateAreaCountyBased() {
         .attr('stroke-width', 3);
 
     chart.select('.line').datum(dataDates)
-    chart.select('.line').datum(dataDates)
         .transition()
         .duration(1000)
         .attr('stroke', Metric.properties[selectedMetric].baseColor)
@@ -277,14 +306,15 @@ async function updateAreaCountyBased() {
             })
             .y(function (d) {
                 if (selectedMetric === 'lethalityRate') {
-                    let rate = data[d][selectedCountyId][Metric.TOTAL_DEATHS] / data[d][selectedCountyId][Metric.TOTAL_CASES];
-                    let value = Math.round(((rate * 100) + Number.EPSILON) * 100) / 100;
-                    return yScaleLine(value);
+                    let rate = getLethalityRate(selectedCountyId, d);
+                    let lethValue = Math.round(((rate * 100) + Number.EPSILON) * 100) / 100;
+                    return yScaleLine(lethValue)
                 } else {
                     return yScaleLine(data[d][selectedCountyId][selectedMetric]);
                 }
             })
         )
+
 }
 
 function setMargin() {
@@ -299,34 +329,48 @@ function setMargin() {
 function getLineDate() {
 
     lineDate = new Date(selectedDate);
-    if (lineDate < minDate) {
-        lineDate = minDate;
-    } else {
-    }
     return lineDate;
 }
 
-function getMaxValue(metric) {
+function getMaxValue() {
     let maxMetricValue;
     maxMetricValue = 0;
-    dataDates = Object.keys(data).filter(item => item === getFormattedDate(minDate) || new Date(item) > new Date(minDate));
+    dataDates = Object.keys(data);
     if (selectedCountyId === null) {
-        maxMetricValue = 0
-    } else if (selectedMetric === 'lethalityRate') {
-        dataDates.forEach(obj => {
-            let lethalityRate = data[obj][selectedCountyId][Metric.TOTAL_DEATHS] / data[obj][selectedCountyId][Metric.TOTAL_CASES];
-            let metricValue = Math.round(((lethalityRate * 100) + Number.EPSILON) * 100) / 100;
-            if (metricValue > maxMetricValue) {
-                maxMetricValue = metricValue;
-            }
-        })
+        if (selectedMetric === 'lethalityRate') {
+            dataDates.forEach(obj => {
+                let lethalityRate = getLethalityRate('all', obj);
+                let metricValue = Math.round(((lethalityRate * 100) + Number.EPSILON) * 100) / 100;
+                if (metricValue > maxMetricValue) {
+                    maxMetricValue = metricValue;
+                }
+            })
+        } else {
+            dataDates.forEach(dataDate => {
+                maxScaleLine = 0;
+                let tempr = data[dataDate]['all'][selectedMetric];
+                if (maxMetricValue < tempr) {
+                    maxMetricValue = tempr;
+                }
+            })
+        }
     } else {
-        dataDates.forEach(obj => {
-            let metricValue = data[obj][selectedCountyId][metric];
-            if (metricValue > maxMetricValue) {
-                maxMetricValue = metricValue;
-            }
-        })
+        if (selectedMetric === 'lethalityRate') {
+            dataDates.forEach(obj => {
+                let lethalityRate = getLethalityRate(selectedCountyId, obj);
+                let metricValue = Math.round(((lethalityRate * 100) + Number.EPSILON) * 100) / 100;
+                if (metricValue > maxMetricValue) {
+                    maxMetricValue = metricValue;
+                }
+            })
+        } else {
+            dataDates.forEach(obj => {
+                let metricValue = data[obj][selectedCountyId][selectedMetric];
+                if (metricValue > maxMetricValue) {
+                    maxMetricValue = metricValue;
+                }
+            })
+        }
     }
     return maxMetricValue;
 }
